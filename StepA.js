ff5176134bd2e0cf2c611d8adf07a9b9e1468d2d
mmf <keon@aayyoo.com>
2019-06-29 10:09:58 -0400

    Added new buttons and features to the StepA screen.

--- StepA.js
+++ StepA.js
@@ -21,11 +21,13 @@ class StepOne extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      susuTitle: '',
-
+      check: true,
+      invitees: [],
     };
     this.handleBack = this.handleBack.bind(this);
     this.handleNext = this.handleNext.bind(this);
+    this.switchStyles = this.switchStyles.bind(this);
+    this.addinvitee = this.addinvitee.bind(this);
   };
 
   handleBack(){
@@ -37,6 +39,16 @@ class StepOne extends Component {
     this.props.navigation.dispatch(backAction);
   };
 
+  switchStyles(){
+    this.setState(prevState => ({
+      check: !prevState.check
+    }));
+  };
+  addinvitee(){
+    //add email address to invitees list
+
+    //For each item in the invitees list, make a little Button that sends its value to this.state.susuEmail and removes it from the list -->
+  };
   componentWillMount (){
 
   };
@@ -46,22 +58,72 @@ class StepOne extends Component {
   };
 
   render(){
+    // This first conditional is for switching the title to
+    //a different style when done
+    let title_display;
+    if (this.state.check){
+      title_display = <View>
+                <Text style={ss.suTitle}>
+                  What do you want to call this Su Su?
+                </Text>
+                <TextInput
+                  style={ss.suTextinput}
+                  onChangeText={(susuTitle) => this.setState({susuTitle})}
+                  value={this.state.susuTitle}
+                  onBlur={this.switchStyles}
+                />
+                </View>;
+    } else {
+      title_display = <View style={{marginTop: '20%',}}>
+                  <TouchableOpacity onPress={this.switchStyles}>
+                  <Text style={ss.sulilSubTitle}>Your $u$u!</Text>
+                  <Text style={ss.sulilTitle}>
+                    <Text style={{color: 'green',}}>-</Text>
+                    {this.state.susuTitle}
+                    <Text style={{color: 'green',}}>-</Text>
+                  </Text>
+                  </TouchableOpacity>
+                </View>;
+    }
+
 
     return(
       <View style={ss.naVcontainer}>
-        <View style={ss.suBc}>
-          <Text style={ss.suTitle}>
-            What do you want to call this SuSu?{"\n"}
+
+        {title_display}
+
+        <Text style={ss.suETitle}>
+            You cannot Susu alone.{'\n'}
+            Invite Someone:
           </Text>
           <TextInput
-            style={ss.suTextinput}
-            onChangeText={(susuTitle) => this.setState({susuTitle})}
-            value={this.state.susuTitle}
+            style={ss.suETextinput}
+            placeholder="example@example.com"
+            placeholderTextColor="grey"
+            onChangeText={(susuEmail) => this.setState({susuEmail})}
+            value={this.state.susuEmail}
+            onBlur={this.addinvitee}
           />
-        </View>
+          {
+            this.state.susuEmail ? (
+              this.state.susuEmail.includes('@') ? (
+                this.state.susuEmail.includes('.') ? (
+                  <TouchableOpacity style={ss.inviteButton} onPress={this.sendInvite}>
+                    <Text style={ss.createSu}>Send</Text>
+                  </TouchableOpacity>
+                ) : null
+              ) : null
+            ): null
+          }
+
+
+
+
+
         <TouchableOpacity style={ss.goBack} onPress={this.handleBack}>
           <Text style={ss.createSu}>Go Back</Text>
         </TouchableOpacity>
+
         <TouchableOpacity style={ss.goNext} onPress={this.handleNext}>
           <Text style={ss.createSu}>Next</Text>
         </TouchableOpacity>
